43ed4cab61ec36aea3eaa5967bf0156071ed838e
Albim360 <alberto.zappala@icloud.com>
2023-02-15 17:27:05 +0100

    FEAT dichiarata let current per prev e next

--- js/app.js
+++ js/app.js
@@ -1,78 +1,85 @@
 const images = [
-    {
-      src: './img/01.jpg',
-      alt: 'Testo alternativo 1',
-      title: 'Titolo slide 1',
-      description: 'Descrizione slide 1'
-    },
-    {
-      src: './img/02.jpg',
-      alt: 'Testo alternativo 2',
-      title: 'Titolo slide 2',
-      description: 'Descrizione slide 2'
-    },
-    {
-      src: './img/03.jpg',
-      alt: 'Testo alternativo 3',
-      title: 'Titolo slide 3',
-      description: 'Descrizione slide 3'
-    },
-    {
-      src: './img/04.jpg',
-      alt: 'Testo alternativo 4',
-      title: 'Titolo slide 4',
-      description: 'Descrizione slide 4'
-    },
-    {
-      src: './img/05.jpg',
-      alt: 'Testo alternativo 5',
-      title: 'Titolo slide 5',
-      description: 'Descrizione slide 5'
-    }
-  ];
+  {
+    src: "./img/01.jpg",
+    alt: "Testo alternativo 1",
+    title: "Titolo slide 1",
+    description: "Descrizione slide 1",
+  },
+  {
+    src: "./img/02.jpg",
+    alt: "Testo alternativo 2",
+    title: "Titolo slide 2",
+    description: "Descrizione slide 2",
+  },
+  {
+    src: "./img/03.jpg",
+    alt: "Testo alternativo 3",
+    title: "Titolo slide 3",
+    description: "Descrizione slide 3",
+  },
+  {
+    src: "./img/04.jpg",
+    alt: "Testo alternativo 4",
+    title: "Titolo slide 4",
+    description: "Descrizione slide 4",
+  },
+  {
+    src: "./img/05.jpg",
+    alt: "Testo alternativo 5",
+    title: "Titolo slide 5",
+    description: "Descrizione slide 5",
+  },
+];
 
-const slidesContainer = document.querySelector('.carosello__slides');
-slidesContainer.innerHTML = '';
+let current = 0
 
-images.forEach(image => {
+const slidesContainer = document.querySelector(".carosello__slides");
+slidesContainer.innerHTML = "";
+
+images.forEach((image) => {
   const slideContent = `
-    <div class="slide">
-      <div class="slide__content">
-        <h2 class="slide__title">${image.title}</h2>
-        <p class="slide__description">${image.description}</p>
+      <div class="slide fade-in">
+        <div class="slide__content">
+          <h2 class="slide__title">${image.title}</h2>
+          <p class="slide__description">${image.description}</p>
+        </div>
+        <img src="${image.src}" alt="${image.alt}">
       </div>
-      <img src="${image.src}" alt="${image.alt}">
-    </div>
-  `;
+    `;
   slidesContainer.innerHTML += slideContent;
 });
 
-const slides = document.querySelectorAll('.slide');
-let current = 0;
+const slides = document.querySelectorAll(".slide");
+slides[0].classList.add("active");
 
 function nextSlide() {
-  slides[current].classList.remove('active');
+  slides[current].classList.remove("active");
+  slides[current].classList.remove("fade-in");
   current = (current + 1) % slides.length;
   slides[current].style.transition = "opacity 0.5s ease-in-out";
   slides[current].style.opacity = 0;
-  setTimeout(function() {
-      slides[current].style.opacity = 1;
+  setTimeout(function () {
+    slides[current].style.opacity = 1;
   }, 100);
-  slides[current].classList.add('active');
+  slides[current].classList.add("active");
+  slides[current].classList.add("fade-in");
 }
 
 function prevSlide() {
-  slides[current].classList.remove('active');
+  slides[current].classList.remove("active");
+  slides[current].classList.remove("fade-in");
   current = (current - 1 + slides.length) % slides.length;
   slides[current].style.transition = "opacity 0.5s ease-in-out";
   slides[current].style.opacity = 0;
-  setTimeout(function() {
-      slides[current].style.opacity = 1;
+  setTimeout(function () {
+    slides[current].style.opacity = 1;
   }, 100);
-  slides[current].classList.add('active');
+  slides[current].classList.add("active");
+  slides[current].classList.add("fade-in");
+  slides[(current + 1) % slides.length].classList.remove("active");
 }
 
-const arrowLeft = document.querySelector('.carosello__arrow.arrow-left');
-const arrowRight = document.querySelector('.carosello__arrow.arrow-right');
-arrowRight.addEventListener('click', nextSlide);
-arrowLeft.addEventListener('click', prevSlide);
+const arrowLeft = document.querySelector(".carosello__arrow.arrow-left");
+const arrowRight = document.querySelector(".carosello__arrow.arrow-right");
+arrowRight.addEventListener("click", nextSlide);
+arrowLeft.addEventListener("click", prevSlide);
